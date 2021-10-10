68aaa72bda6c0d506c08c8e9e1ddfed33abbea51
rongcl <963941583@qq.om>
2021-10-10 16:48:32 +0800

    修改关于页面

--- src/views/home/About/index.vue
+++ src/views/home/About/index.vue
@@ -1,30 +1,11 @@
 <template>
     <div>
-        <button @click="setTheme('light')">红色主题</button>
-        <button @click="setTheme('dark')">蓝色主题</button>
-        <hr />
-        <h1 class="them_title">This is an about page</h1>
-        <button class="them-btn-default">默认</button>
-        <button class="them-btn-primary">主要</button>
-        <button class="them-btn-info">提示</button>
+        <NoData/>
     </div>
 </template>
 
 <script>
-import {mapActions} from 'vuex'
 export default {
-    name: "About",
-    methods: {
-        ...mapActions('setting', [
-            'setTheme'
-        ])
-    }
+    name: "About"
 }
 </script>
-
-<style scoped lang="scss">
-.them_title {
-    font-size: 20px;
-    @include font_color("color-primary");
-}
-</style>
