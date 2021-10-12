5518efe2c08d183df2e0f03b1ac40fcd61c072c2
rongcl <963941583@qq.com>
2021-10-12 15:46:07 +0800

    修改cdn引入

--- config/cdn.config.js
+++ config/cdn.config.js
@@ -1,11 +1,16 @@
 module.exports = {
     css: [
+        "https://unpkg.com/element-ui@2.15.5/lib/theme-chalk/index.css",
         "https://cdn.bootcss.com/github-markdown-css/4.0.0/github-markdown.min.css",
         "https://unpkg.com/@highlightjs/cdn-assets@11.2.0/styles/github.min.css",
         "https://unpkg.com/mavon-editor@2.9.1/dist/css/index.css",
         "https://unpkg.com/vue-photo-preview@1.1.3/dist/skin.css"
     ],
     js: [
+        "https://unpkg.com/vue@2.6.11/dist/vue.min.js",
+        "https://unpkg.com/vuex@3.4.0/dist/vuex.min.js",
+        "https://unpkg.com/vue-router@3.2.0/dist/vue-router.min.js",
+        "https://unpkg.com/element-ui@2.15.5/lib/index.js",
         "https://unpkg.com/mavon-editor@2.9.1/dist/mavon-editor.js",
         "https://unpkg.com/@highlightjs/cdn-assets@11.2.0/highlight.min.js",
         "https://cdn.bootcdn.net/ajax/libs/marked/3.0.4/marked.min.js",
