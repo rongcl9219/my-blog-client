a2183487b3d819e9099e79e1ccd1e1352ee0c147
rongcl <963941583@qq.om>
2021-10-13 20:36:56 +0800

    修改备案号

--- src/components/layout/home/Footer/index.vue
+++ src/components/layout/home/Footer/index.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="footer">
-        <p>本站由 @RongCL 创建 - © 2020. <a href="http://beian.miit.gov.cn/" target="_blank">京ICP备2020038858号-1</a></p>
+        <p>本站由 @RongCL 创建 - © 2021. <a href="http://beian.miit.gov.cn/" target="_blank">皖ICP备2021014369号-1</a></p>
     </footer>
 </template>
 
